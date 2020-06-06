c3d4fc52412578893aa7502f521e1b97ff638b59
Tim Deichsel <Daywalker753@aol.com>
2020-06-06 12:20:24 +0200

    - Added Settings window - added capability to change words per minute (speed setting)

--- Speedreader/MainWindow.xaml.cs
+++ Speedreader/MainWindow.xaml.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Speedreader.Properties;
+using System;
 using System.Collections.Generic;
 using System.Data.SqlTypes;
 using System.Diagnostics.Eventing.Reader;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
@@ -26,39 +28,41 @@ namespace Speedreader
     public partial class MainWindow : Window
     {
         DispatcherTimer dispatcherTimer = new DispatcherTimer();
+        private static SettingsWindow settingsWin;
 
         bool isInReadMode;
         bool isSeetingsOpen;
 
-        int readingSpeed = 400;
         private static int position = 0;
         List<string> words = new List<string>();
+
+        public static MainWindow mainWindow;
+
+        private int readSpeed = 200;
+        public int ReadSpeed
+        {
+            get { return readSpeed; }
+            set
+            {
+                readSpeed = value;
+                SetReadingSpeed(readSpeed);
+            }
+        }
+
         public MainWindow()
         {
+            mainWindow = this;
+
             InitializeComponent();
-            //speedTextBox.Text = readingSpeed.ToString();
 
             dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
             StartStopBtn.Content = "Start";
             MenuBtn.Content = "Settings";
         }
 
-        //private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
-        //{
-        //    Regex regex = new Regex("[0-9]+");
-        //    if(regex.IsMatch(speedTextBox.Text))
-        //    {
-        //        readingSpeed = int.Parse(speedTextBox.Text);
-        //    } else
-        //    {
-        //        speedTextBox.Text = readingSpeed.ToString();
-        //    }
-        //}
-
-        private void SetReadingSpeed(int valueChange)
+        public void SetReadingSpeed(int value)
         {
-            readingSpeed = readingSpeed + valueChange;
-            int speed = 60000 / readingSpeed;
+            int speed = 60000 / value;
             dispatcherTimer.Interval = new TimeSpan(0, 0, 0, 0, speed);
         }
 
@@ -80,7 +84,7 @@ namespace Speedreader
         {
             ReadMode();
             SpeedReaderPoint.Content = words[position];
-            if (position < words.Count)
+            if (position < words.Count -1)
             {
                 position++;
             }
@@ -103,8 +107,8 @@ namespace Speedreader
                 string text = GetStringFromRichTextBox(InputTextBox);
                 text = text.Replace(System.Environment.NewLine, "");
 
+                SetReadingSpeed(ReadSpeed);
                 words = text.Split(' ').ToList<string>();
-                SetReadingSpeed(0);
                 dispatcherTimer.Start();
             }            
         }
@@ -117,7 +121,32 @@ namespace Speedreader
 
         private void MenuBtn_Click(object sender, RoutedEventArgs e)
         {
+            if (!isSeetingsOpen)
+            {
+                isSeetingsOpen = true;
+                settingsWin = new SettingsWindow();
 
+
+                Point targetPoint = GetScreenPointOfControl(MenuBtn);
+                settingsWin.WindowStartupLocation = WindowStartupLocation.Manual;
+                settingsWin.Left = targetPoint.X+20;
+                settingsWin.Top = targetPoint.Y;
+
+                settingsWin.Show();
+            } 
+            else
+            {
+                isSeetingsOpen = false;
+                settingsWin.Close();
+                settingsWin = null;
+            }
+        }
+
+        private Point GetScreenPointOfControl(Control control)
+        {
+            PresentationSource source = PresentationSource.FromVisual(this);
+            System.Windows.Point targetPoints = source.CompositionTarget.TransformFromDevice.Transform(control.PointToScreen(new Point(0, 0)));
+            return targetPoints;
         }
     }
 }
---
+++ Speedreader/SettingsWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Speedreader
+{
+    /// <summary>
+    /// Interaction logic for SettingsWindow.xaml
+    /// </summary>
+    public partial class SettingsWindow : Window
+    {
+        public SettingsWindow()
+        {
+            InitializeComponent();
+            WpmTxtbox.Text = MainWindow.mainWindow.ReadSpeed.ToString();
+            
+        }
+
+        private void WinSizeCmbBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void FontSizeCmbBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void FontColorBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BgColorBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void WpmTxtbox_OnFocusLost(object sender, RoutedEventArgs e)
+        {
+            Regex regex = new Regex("[0-9]+");
+            if (regex.IsMatch(WpmTxtbox.Text))
+            {
+                MainWindow.mainWindow.ReadSpeed = int.Parse(WpmTxtbox.Text);
+            }
+            else
+            {
+                WpmTxtbox.Text = MainWindow.mainWindow.ReadSpeed.ToString();
+            }
+        }
+    }
+}
